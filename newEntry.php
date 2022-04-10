d4db150a91378e759f899f7d9603edb81144ad13
Amilcar Aponte <apontejaj@gmail.com>
2022-04-10 19:18:30 +0100

    Saving data to DB

--- newEntry.php
+++ newEntry.php
@@ -6,31 +6,25 @@
     // echo $_POST['name'];
     // echo $_POST['content'];
 
+    try {
+        $host = $SS_DB_HOST;
+        $dbname = $SS_DB_NAME;
+        $dbuser = $SS_DB_USER;
+        $pass = $SS_DB_PASSWORD;
+        # MySQL with PDO_MYSQL
+        $DBH = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $pass);
+
+        $DBH->query("INSERT INTO forum (author, post) VALUES ('". $_POST['name'] ."','". $_POST['content'] ."');");
+
+        $DBH = null;
+
+    } catch(PDOException $e) {echo $e->getMessage();}  
+
     header("Location: index.php");
     die();
     
     
-    // try {
-    //     $host = $SS_DB_HOST;
-    //     $dbname = $SS_DB_NAME;
-    //     $dbuser = $SS_DB_USER;
-    //     $pass = $SS_DB_PASSWORD;
-    //     # MySQL with PDO_MYSQL
-    //     $DBH = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $pass);
 
-        
-    //     foreach ($DBH->query('SELECT * FROM forum;') as $row){
-    //         echo '<tr>';            
-    //         echo '<td>' . $row['date_posted'] . '</td>';
-    //         echo '<td>' . $row['author'] . '</td>';
-    //         echo '<td>' . $row['post'] . '</td>';
-    //         echo '</tr>';
-    //     }
-        
-
-    //     $DBH = null;
-
-    // } catch(PDOException $e) {echo $e->getMessage();}  
 
     // $sql = "SELECT * FROM `switch` WHERE `user` = ?;";
     // $sth = $DBH->prepare($sql);
