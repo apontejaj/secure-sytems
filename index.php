7e1b10ac2f1728b0f64c91faf53e859058a1ed2f
Amilcar Aponte <apontejaj@gmail.com>
2022-04-10 15:13:29 +0100

    Testing Environment Variables

--- index.php
+++ index.php
@@ -1,6 +1,38 @@
+<h1> hello </h1>
+
+
 <?php 
 
-<h1> hello </h1>
+    echo getenv('SS_DB_HOST');
+
+
+// try {
+//     $host = 'apontejaj.com';
+//     $dbname = 'switch';
+//     $dbuser = 'root';
+//     $pass = 'Pass1234!';
+//     # MySQL with PDO_MYSQL
+//     $DBH = new PDO("mysql:host=$host;dbname=$dbname", $dbuser, $pass);
+// } catch(PDOException $e) {echo 'Error';}  
+
+// $sql = "SELECT * FROM `switch` WHERE `user` = ?;";
+// $sth = $DBH->prepare($sql);
+
+// $sth->bindParam(1, $user, PDO::PARAM_INT);
+
+// $sth->execute();
+
+// $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+// $allPorts = '{';
+// foreach($result as $item){
+//     $allPorts = $allPorts . '"port' . $item['port'] . '":"' . $item['status'] . '",';
+// }
+// $allPorts = $allPorts . '"coder":"amilcar"}';
+// echo $allPorts;
+
+// //$status = $result['status'];
+
+
 
 
 
